54674dcd025d6523f2ac7d3ac88a46275849003d
Tim Loderhose <tim@loderhose.com>
2023-02-26 18:12:25 +0900

    Start working on better implementation
    - catch-all commit because everything changes all the time

--- loopmate/loop.py
+++ loopmate/loop.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import asyncio
 import queue
 import sys
@@ -5,7 +7,7 @@ import termios
 import threading
 import time
 import tty
-from dataclasses import dataclass
+from dataclasses import KW_ONLY, dataclass
 from enum import Enum, member
 from pathlib import Path
 from typing import Callable
@@ -14,11 +16,21 @@ import matplotlib.pyplot as plt
 import mido
 import numpy as np
 import pandas as pd
+import pedalboard
 import soundcard as sc
 import sounddevice as sd
 import soundfile as sf
 import typer
 from scipy import signal as sig
+
+from . import config
+
+# TODO:
+#
+# - wrapper class around tasks that handles e.g. a full mute, with fade in/mute
+# - don't use action queue the way it currently works, or add some outer list
+# - that holds current tasks such that they can be selected
+# - allow tasks to be cancelled immediately / in the next callback
 
 
 def chirp(
@@ -40,106 +52,74 @@ def chirp(
     return a * sig.chirp(t, f0, l, f1, method=method, phi=phi)
 
 
-def make_loop_callback(data: np.ndarray):
-    """TODO
-
-    Can probably add audio transformations in here as well, and interpolate
-    based on some temparature and the current time in the loop/session.
-
-    Every loop should be able to change state.
-
-    TODO: MUTE, RESTART (on trigger)
-
-    :param data: audio data to loop
-
-    :returns:
-    """
-
-    n = len(data)
-    current_frame = 0
-
-    def callback(outdata, frames, time, status):
-        if status:
-            print(status)
-
-        leftover = n - current_frame
-        chunksize = min(leftover, frames)
-        outdata[:chunksize] = data[current_frame : current_frame + chunksize]
-        # to loop:
-        if leftover < frames:
-            outdata[chunksize:] = data[: frames - leftover]
-            current_frame = frames - leftover
-        else:
-            current_frame += chunksize
-
-    return callback
+blend_windowsize = int(config.blend_length * config.sr)
+RAMP = np.linspace(1, 0, blend_windowsize)
 
 
-device = "default"
-latency = 0.002
-blocksize = 256
-
-# Precompute windows for pop protection
-windowsize = min(blocksize, 128)
-windows = {
-    i: sig.windows.hann(
-        i * 2,
-    )[:, None]
-    for i in range(windowsize + 1)
-}
-
-
-def no_pop(buffer, pos=0, up=True):
-    n = len(buffer)
-    length = min(windowsize, n - pos if up else pos)
-    if up:
-        buffer[pos : pos + length] *= windows[length][:length]
+def blend(x, n=None):
+    if n is None:
+        ramp = np.linspace(1, 0, n)
     else:
-        buffer[pos - length : pos] *= windows[length][-length:]
+        ramp = RAMP
+    return ramp + x - ramp * x
 
 
-class Tasks(Enum):
-    @member
-    def p():
-        print("Hi")
-
-    @member
-    def a():
-        print("a")
+def blend2(x1, x2, n=None):
+    if n is None:
+        ramp = np.linspace(1, 0, n)
+    else:
+        ramp = RAMP
+    return ramp * x1 + (1 - ramp) * x2
 
 
-class Task:
-    # Something that can modify streams/callbacks within streams
-    pass
+# A mute could just be a 0 multiplier that is blended in/out
+# Make sure the blend is shorter than the audio
 
 
 @dataclass
-class TimedTask:
-    # Work in samples or seconds? probably samples is better
-    start: float
-    end: float
-    sr: int
-    task: Tasks
-    # Should
+class Action:
+    start: int
+    end: int
 
+    _: KW_ONLY
+    recurring: bool = False
+    priority: int = 3
+    # Consuming this action will 'spawn'/queue this new action
+    spawn: Action | None = None
 
-async def worker(loop, q):
-    # Perhaps give each task a time/event when it should be triggered, then use
-    # asyncio to actually execute once that's the case? E.g., for reverse, we
-    # have to reverse the audio before we actually restart the loop, otherwise
-    # it will probably reverse on the second frame. For restart, we may also
-    # want to trigger it when a timer counts down, and then put it in the
-    # queue. Ergo, we need timekeeping somehow.
-    while True:
-        task = await q.get()
-        # Do task
-        if task == "start":
-            loop.start()
-        elif task == "stop":
-            loop.stop()
-        elif task == "reverse":
-            pass
-        q.task_done()
+    def __post_init__(self):
+        self.n = self.end - self.start
+        # Current sample !inside action between start and end
+        # don't mix up with current_frame!
+        self.current_sample = 0
+        self.consumed = False
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current_sample >= self.n:
+            if self.recurring:
+                self.current_sample = 0
+            raise StopIteration
+        return self.do
+
+    def run(self, data):
+        self.do(data)
+        if self.recurring:
+            self.current_sample = 0
+
+        if self.current_sample >= self.n:
+            self.consumed = True
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+    def do(self, outdata):
+        raise NotImplementedError("Subclasses need to inherit this!")
+
+    def set_priority(self, priority):
+        self.priority = priority
 
 
 @dataclass
@@ -151,9 +131,18 @@ class Loop:
     _reverse: bool = False
     current_frame: int = 0
     sr: int = 96000
+    pop_window_ms: float = 0.005
 
     def __post_init__(self):
+        # Remove pop at loop edge
+        pop_window = int(self.sr * self.pop_window_ms)
+        window = sig.windows.hann(pop_window)[:, None]
+        self.audio[: pop_window // 2] *= window[: pop_window // 2]
+        if pop_window > 0:
+            self.audio[-(pop_window // 2) :] *= window[-(pop_window // 2) :]
+
         self.n = len(self.audio)
+        self.n_frames = np.ceil(self.n / blocksize)
         self.stream = sd.OutputStream(
             samplerate=self.sr,
             device=device,
@@ -164,30 +153,10 @@ class Loop:
         )
         self.trans_left = False
         self.trans_right = False
-        self.tasks = asyncio.Queue()
+        self.raudio = self.audio[::-1]
+        self._current_frame_i = self.current_frame // blocksize
 
-    def reverse(self):
-        self.audio = self.audio[::-1]
-        self._reverse = not self._reserve
-
-    async def loop_start(self, outdata, start):
-        """Empties task queue when
-
-        :param outdata:
-        :param start:
-        :returns:
-
-        """
-        # This should be called whenever the loop is reset; the task queue will
-        # be emptied
-
-        # Reduce pop - perhaps don't do this here, and just empty the task
-        # queue
-        no_pop(outdata, start, up=True)
-        task = asyncio.create_task(worker(self.__repr__(), self.tasks))
-        await self.tasks.join()
-        task.cancel()
-        await task
+        self.actions = Actions(self)
 
     def _get_callback(self):
         """
@@ -197,61 +166,51 @@ class Loop:
         def callback(outdata, frames, time, status):
             if status:
                 print(status)
-            # if self.stop:
-            #     self.stop = False
-            #     raise sd.CallbackStop
 
             leftover = self.n - self.current_frame
-
-            print(f"playing at {self.current_frame}")
-            if self.mute:
-                outdata[:] = 0.0
-                if leftover <= frames:
-                    self.current_frame = frames - leftover
-                else:
-                    self.current_frame += frames
-                return
-
             chunksize = min(leftover, frames)
+            next_frame = self.current_frame + chunksize
+
+            # print(f"playing at {self.current_frame}")
+            # print(time.currentTime - time.outputBufferDacTime)
+            # TODO: move into action after buffer is filled
+            # if self.mute:
+            #     outdata[:] = 0.0
+            #     if leftover <= frames:
+            #         self.current_frame = frames - leftover
+            #     else:
+            #         self.current_frame += frames
+            #     return
 
             outdata[:chunksize] = self.audio[
                 self.current_frame : self.current_frame + chunksize
             ]
-            if self.trans_left:
-                no_pop(outdata, 0, True)
-                self.trans_left = False
-            if self.trans_right:
-                no_pop(outdata, chunksize - windowsize, False)
-                self.trans_right = False
-            # To loop, add start of audio to end of output buffer:
             if leftover <= frames:
                 outdata[chunksize:] = self.audio[: frames - leftover]
-                no_pop(outdata, chunksize, True)
-                no_pop(outdata, chunksize, False)
+
+            self.actions.run(outdata, self.current_frame)
+            # To loop, add start of audio to end of output buffer:
+            if leftover <= frames:
                 self.current_frame = frames - leftover
+                self._current_frame_i = 0
             else:
                 self.current_frame += frames
-
-            # Last frame fit end of audio perfectly, we need to unpop left next
-            if self.current_frame == 0:
-                self.trans_left = True
-
-            # Transformations:
-            outdata[:] = self.transformation(self.gain * outdata)
+                self._current_frame_i += 1
 
         return callback
 
     async def start(self):
         print("starting stream")
-        self.trans_left = True
+        self.current_frame = 0
         self.stream.start()
 
     async def stop(self, reset=True):
         print("stopping stream")
-        if reset:
-            self.current_frame = 0
-        self.trans_right = True
-        self.stream.stop()
+        # if reset:
+        #     self.current_frame = 0
+        # self.trans_right = True
+        # self.stream.stop()
+        self.actions.stop = True
 
 
 class CharIn:
@@ -286,6 +245,15 @@ async def main():
                 await loop.start()
             if c == "m":
                 loop.mute = not loop.mute
+                print(loop.mute)
+                if loop.mute:
+                    print("MUTING")
+                    loop.actions.q.put_nowait(
+                        Mute3(loop.current_frame, loop.n)
+                    )
+                else:
+                    mute = loop.actions.q.get_nowait()
+                    mute.unmute(loop.current_frame)
             if c == "o":
                 await loop.stop()
             if c == "r":
@@ -294,8 +262,15 @@ async def main():
                 loop.gain = 0.5
             if c == "b":
                 loop.gain = 1.0
+            if c == "c":
+                # schedule stop task at end of loop
+                pass
+                # loop.tasks.put(loop.stop_next)
             if c == "q":
                 break
+    except (sd.CallbackStop, sd.CallbackAbort):
+        print("Stopped")
+
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 

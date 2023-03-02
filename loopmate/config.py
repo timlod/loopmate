c5f4a1e8410b8ba7b39caa1e12abf4cee744acb2
Tim Loderhose <tim@loderhose.com>
2023-03-02 11:11:09 +0900

    Use interface

--- loopmate/config.py
+++ loopmate/config.py
@@ -1,12 +1,15 @@
 # Configuration for loopmate
 
-sr = 96000
+# Global sample rate for all audio being played/recorded - if loading different
+# SR files, convert them
+sr = 48000
 device = "default"
+device = "USB AUDIO CODEC"
 # Desired latency of audio interface, in ms
 latency = 0.002
 # Blocksize to use in processing, the lower the higher the CPU usage, and lower
 # the latency
-blocksize = 512
+blocksize = 256
 # Length (in ms) of blending window, e.g. used to remove pops in muting, or
 # applying transformations to audio
 blend_length = 0.005
